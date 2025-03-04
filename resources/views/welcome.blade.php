32ec1fdf13f6d81823afb36ddd6c7f2274791e1d
tsrgtm <gautamtusar2@gmail.com>
2025-03-04 23:58:25 +0545

    I don't know what I am doing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @fluxAppearance
     </head>
