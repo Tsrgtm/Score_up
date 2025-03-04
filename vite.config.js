3020ec3ac7a7361ba2775304a0a769b478b19ca5
tsrgtm <gautamtusar2@gmail.com>
2025-03-05 01:17:04 +0545

    added force https connection

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     server: {
-        // Listen on all addresses inside the container
-        host: true,
-        hmr: {
-            // Force the Vite client to connect via SSL
-            // This will also force a "https://" URL in the hot file 
-            protocol: 'wss',
-            // The host where the Vite dev server can be accessed
-            // This will also force this host to be written to the hot file
-            host: 'tsrgtm.tech',
-        },
+        https: true,
+        host: '0.0.0.0'
     },
     plugins: [
         basicSsl(),
