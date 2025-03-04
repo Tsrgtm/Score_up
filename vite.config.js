a04847530d7d905c6125ed6aaaae5910c84d4bcb
tsrgtm <gautamtusar2@gmail.com>
2025-03-05 00:38:05 +0545

    added force https connection

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        https: true,
+        host: '0.0.0.0'
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
