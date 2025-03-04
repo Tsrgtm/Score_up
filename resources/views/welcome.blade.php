66fb97bf8bea10407e1ac235e4efa227a2318a07
tsrgtm <gautamtusar2@gmail.com>
2025-03-05 00:11:05 +0545

    Added tailwind cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @fluxAppearance
     </head>
