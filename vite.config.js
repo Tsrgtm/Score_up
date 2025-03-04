960ece7f2cb57735069dec796497b928811a8bc9
tsrgtm <gautamtusar2@gmail.com>
2025-03-05 02:11:04 +0545

    added force https connection

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    server: {
+        hmr: {
+            protocol: 'wss',
+            host: 'tsrgtm.tech',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
